c6012a7e99a8078f1ed279c094b3969d2517fb44
PRINT3R <brrr@print3r.xyz>
2023-11-06 17:23:16 +0000

    Added Swaps

--- src/markets/MarketToken.sol
+++ src/markets/MarketToken.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.20;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {RoleValidation} from "../access/RoleValidation.sol";
 
-/// @dev Only Liquidity Vault can mint and burn tokens
+/// @dev Only Vaults can mint and burn tokens
 contract MarketToken is ERC20("MarketToken", "MKT"), RoleValidation {
     constructor() RoleValidation(roleStorage) {}
 
