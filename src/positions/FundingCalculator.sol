925521470161b886535da48aa2bedaed0c1dceb5
danieljoseph18 <danieljoseph1805@gmail.com>
2023-12-12 12:40:46 +0000

    Fixed Funding Calculations

--- src/positions/FundingCalculator.sol
+++ src/positions/FundingCalculator.sol
@@ -89,57 +89,74 @@ library FundingCalculator {
     ) internal pure returns (uint256 longFees, uint256 shortFees) {
         // Calculate final funding rate after time elapsed
         int256 finalFundingRate = _fundingRate + _fundingRateVelocity * int256(_timeElapsed);
+        bool flipsSign = (_fundingRate >= 0 && finalFundingRate < 0) || (_fundingRate < 0 && finalFundingRate >= 0);
+        bool crossesBoundary = finalFundingRate > _maxFundingRate || finalFundingRate < _minFundingRate;
 
-        // Check for boundary crossing and sign flip
-        if (finalFundingRate > _maxFundingRate || finalFundingRate < _minFundingRate) {
-            // Case 1: Crossing the boundary
-            uint256 timeToBoundary;
-            int256 boundaryRate;
-
-            // Determine the time to reach the boundary and which boundary is hit
-            if (finalFundingRate > _maxFundingRate) {
-                timeToBoundary = uint256((_maxFundingRate - _fundingRate) / _fundingRateVelocity);
-                boundaryRate = _maxFundingRate;
-            } else {
-                timeToBoundary = uint256((_minFundingRate - _fundingRate) / _fundingRateVelocity);
-                boundaryRate = _minFundingRate;
-            }
-
-            // Calculate funding until hitting the boundary
-            int256 fundingUntilBoundary = _calculateSeriesSum(_fundingRate, _fundingRateVelocity, timeToBoundary);
-
-            // Calculate funding after hitting the boundary
-            uint256 timeAfterBoundary = _timeElapsed - timeToBoundary;
-            int256 fundingAfterBoundary = boundaryRate * int256(timeAfterBoundary);
-
-            return boundaryRate == _maxFundingRate
-                ? (uint256(fundingUntilBoundary) + uint256(fundingAfterBoundary), uint256(0)) // Positive Case
-                : (uint256(0), uint256(fundingUntilBoundary) + uint256(fundingAfterBoundary)); // Negative Case
-        } else if ((_fundingRate >= 0 && finalFundingRate < 0) || (_fundingRate < 0 && finalFundingRate >= 0)) {
-            // Case 2: Sign flip
-            // Calculate time to sign flip
-            uint256 timeToFlip = uint256(-_fundingRate / _fundingRateVelocity);
-
-            // Calculate funding until the sign flip
-            int256 fundingUntilFlip = _calculateSeriesSum(_fundingRate, _fundingRateVelocity, timeToFlip);
-
-            // Remaining time after the flip
-            uint256 timeAfterFlip = _timeElapsed - timeToFlip;
-
-            // Calculate funding after the flip with the flipped rate
-            int256 flippedRate = _fundingRate + _fundingRateVelocity * int256(timeToFlip);
-            int256 fundingAfterFlip = flippedRate * int256(timeAfterFlip);
-
-            return fundingAfterFlip >= 0
-                ? (uint256(fundingAfterFlip), uint256(fundingUntilFlip)) // Positive Case
-                : (uint256(fundingUntilFlip), uint256(fundingAfterFlip)); // Negative Case
+        if (crossesBoundary && flipsSign) {
+            return _calculateForDoubleCross(_fundingRate, _fundingRateVelocity, _maxFundingRate, _timeElapsed);
+        } else if (crossesBoundary) {
+            return _calculateForBoundaryCross(_fundingRate, _fundingRateVelocity, _maxFundingRate, _timeElapsed);
+        } else if (flipsSign) {
+            return _calculateForSignFlip(_fundingRate, _fundingRateVelocity, _timeElapsed);
         } else {
-            // Case 3: No boundary crossing or sign flip, use standard calculation\
             int256 fee = _calculateSeriesSum(_fundingRate, _fundingRateVelocity, _timeElapsed);
-            return fee >= 0
-                ? (uint256(fee), uint256(0)) // Positive Case
-                : (uint256(0), uint256(-fee)); // Negative Case
+            return fee >= 0 ? (uint256(fee), uint256(0)) : (uint256(0), uint256(-fee));
         }
+    }
+
+    function _calculateForDoubleCross(
+        int256 _fundingRate,
+        int256 _fundingRateVelocity,
+        int256 _maxFundingRate,
+        uint256 _timeElapsed
+    ) internal pure returns (uint256 longFees, uint256 shortFees) {
+        uint256 absRate = _fundingRate > 0 ? uint256(_fundingRate) : uint256(-_fundingRate);
+        uint256 absVelocity = _fundingRateVelocity > 0 ? uint256(_fundingRateVelocity) : uint256(-_fundingRateVelocity);
+        uint256 timeToFlip = absRate / absVelocity;
+        uint256 timeToBoundary = _fundingRate > 0
+            ? uint256((_maxFundingRate + _fundingRate)) / absVelocity
+            : uint256((_maxFundingRate + -_fundingRate)) / absVelocity;
+        int256 fundingUntilFlip = _calculateSeriesSum(_fundingRate, _fundingRateVelocity, timeToFlip);
+        int256 fundingRateAfterFlip = _fundingRate + (_fundingRateVelocity * int256(timeToFlip));
+        int256 fundingUntilBoundary =
+            _calculateSeriesSum(fundingRateAfterFlip, _fundingRateVelocity, timeToBoundary - timeToFlip);
+        int256 fundingAfterBoundary = _maxFundingRate * int256(_timeElapsed - timeToBoundary);
+        return _fundingRate >= 0
+            ? (uint256(fundingUntilFlip), uint256(fundingUntilBoundary) + uint256(fundingAfterBoundary))
+            : (uint256(fundingUntilBoundary) + uint256(fundingAfterBoundary), uint256(fundingUntilFlip));
+    }
+
+    function _calculateForBoundaryCross(
+        int256 _fundingRate,
+        int256 _fundingRateVelocity,
+        int256 _maxFundingRate,
+        uint256 _timeElapsed
+    ) internal pure returns (uint256 longFees, uint256 shortFees) {
+        uint256 absRate = _fundingRate > 0 ? uint256(_fundingRate) : uint256(-_fundingRate);
+        uint256 absVelocity = _fundingRateVelocity > 0 ? uint256(_fundingRateVelocity) : uint256(-_fundingRateVelocity);
+        uint256 timeToBoundary = (uint256(_maxFundingRate) - absRate) / absVelocity;
+        int256 fundingUntilBoundary = _calculateSeriesSum(_fundingRate, _fundingRateVelocity, timeToBoundary);
+        int256 fundingAfterBoundary = _maxFundingRate * int256(_timeElapsed - timeToBoundary);
+        return _fundingRate >= 0
+            ? (uint256(fundingUntilBoundary) + uint256(fundingAfterBoundary), uint256(0))
+            : (uint256(0), uint256(fundingUntilBoundary) + uint256(fundingAfterBoundary));
+    }
+
+    function _calculateForSignFlip(int256 _fundingRate, int256 _fundingRateVelocity, uint256 _timeElapsed)
+        internal
+        pure
+        returns (uint256 longFees, uint256 shortFees)
+    {
+        uint256 absRate = _fundingRate > 0 ? uint256(_fundingRate) : uint256(-_fundingRate);
+        uint256 absVelocity = _fundingRateVelocity > 0 ? uint256(_fundingRateVelocity) : uint256(-_fundingRateVelocity);
+        uint256 timeToFlip = absRate / absVelocity;
+        int256 fundingUntilFlip = _calculateSeriesSum(_fundingRate, _fundingRateVelocity, timeToFlip);
+        int256 fundingRateAfterFlip = _fundingRate + (_fundingRateVelocity * int256(timeToFlip));
+        int256 fundingAfterFlip =
+            _calculateSeriesSum(fundingRateAfterFlip, _fundingRateVelocity, _timeElapsed - timeToFlip);
+        return _fundingRate >= 0
+            ? (uint256(fundingUntilFlip), uint256(fundingAfterFlip))
+            : (uint256(fundingAfterFlip), uint256(fundingUntilFlip));
     }
 
     /// @dev Calculates the sum of an arithmetic series.
@@ -161,7 +178,7 @@ library FundingCalculator {
         }
 
         int256 startRate = _fundingRate;
-        int256 endRate = _fundingRate + _fundingRateVelocity * int256(_timeElapsed - 1);
+        int256 endRate = _fundingRate + (_fundingRateVelocity * int256(_timeElapsed - 1));
 
         return int256(_timeElapsed) * (startRate + endRate) / 2;
     }
